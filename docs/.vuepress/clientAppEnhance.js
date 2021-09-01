d1f850e61f84838f4e2c2f69f8ef269d3c7d3784
liziwl <leezisy@gmail.com>
2021-09-01 11:35:37 +0800

    add 百度统计

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -1,4 +1,5 @@
 import { defineClientAppEnhance } from '@vuepress/client'
+import baiduAnalytics from 'vue-baidu-analytics'
 
 import BusTimer from "./components/BusTimer.vue";
 import ObjectSelector from "./components/ObjectSelector.vue";
@@ -6,8 +7,15 @@ import DataRequest from "./components/DataRequest.vue";
 import GridList from "./components/GridList.vue";
 
 export default defineClientAppEnhance(({ app, router, siteData }) => {
-    app.component("BusTimer",BusTimer)
-    app.component("ObjectSelector",ObjectSelector)
-    app.component("DataRequest",DataRequest)
-    app.component("GridList",GridList)
+    app.component("BusTimer", BusTimer)
+    app.component("ObjectSelector", ObjectSelector)
+    app.component("DataRequest", DataRequest)
+    app.component("GridList", GridList)
+    app.use(baiduAnalytics, {
+        router: router,
+        siteIdList: [
+            'd3d2c23173a08119fe3e95b5f4c19b02'
+        ],
+        isDebug: false
+    });
 })
