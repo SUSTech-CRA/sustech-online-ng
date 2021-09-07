85d43dcf1b1781a921b43218a03a9ea9a62e81fb
liziwl <leezisy@gmail.com>
2021-09-08 00:48:32 +0800

    try add google adsense

--- docs/.vuepress/public/wx_helper.js
+++ docs/.vuepress/public/wx_helper.js
@@ -77,3 +77,16 @@ function reset_all_anchor() {
 }
 
 setInterval(reset_all_anchor, 1000);
+
+function load_adsense() {
+    if (!window.is_miniprogram) {
+        console.log("非小程序环境，加载 adsense。")
+        var oScript = document.createElement("script");
+        oScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+        oScript.setAttribute("async", "");
+        oScript.setAttribute("data-ad-client", "ca-pub-9039393129169217");
+        document.body.appendChild(oScript);
+        // <script data-ad-client="ca-pub-9039393129169217" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    }
+}
+setTimeout("load_adsense()", 500);
