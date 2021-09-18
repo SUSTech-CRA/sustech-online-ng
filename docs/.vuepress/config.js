c8d7f44cd20d322ec61a8e3d36f97411d2bf5ca3
sparkcyf <sparktour@outlook.com>
2021-09-18 13:31:27 +0800

    fix ad style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -108,7 +108,7 @@ module.exports = {
       {
         components: {
           Realtimemap: path.resolve(__dirname, './components/Realtime-map.vue'),
-          Adsense_unit: path.resolve(__dirname, './components/adsense-inline.vue'),
+          Adsense_unit: path.resolve(__dirname, './components/adsense-inline-article.vue'),
           // componentsDir: path.resolve(__dirname, './components')
 
         },
