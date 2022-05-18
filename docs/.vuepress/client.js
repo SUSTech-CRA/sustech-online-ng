eb9ff60a40780bbbf95d162b6693c175908f9f99
liziwl <leezisy@gmail.com>
2022-05-18 09:58:02 +0800

    fix #108，解决上游兼容性问题

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,13 +1,14 @@
-import {defineClientAppEnhance} from '@vuepress/client'
-
+import { defineClientConfig } from '@vuepress/client'
 import BusTable from "./components/BusTable.vue";
 import Realtimemap from './components/Realtime-map.vue'
 import WeatherSpan from './components/weather-span.vue'
 // import AdsenseUnit from './components/adsense-inline-article.vue'
 
-export default defineClientAppEnhance(({app, router, siteData}) => {
+export default defineClientConfig({
+  enhance({ app }) {
     app.component("BusTable", BusTable)
     app.component("Realtimemap", Realtimemap)
     app.component("WeatherSpan", WeatherSpan)
     // app.component("AdsenseUnit", AdsenseUnit)
+  },
 })
