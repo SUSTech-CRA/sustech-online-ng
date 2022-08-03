57c85af6040ed9497baac35dbde57dbc681b0963
liziwl <leezisy@gmail.com>
2022-08-03 22:49:38 +0800

    can run

---
+++ docs/.vuepress/components/BusChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <BasicPure></BasicPure>
+</template>
+<script>
+import {applyPureReactInVue} from 'veaury'
+import BasicReactComponent from './bus_ni1o1/Basic'
+
+export default {
+  components: {
+    BasicPure: applyPureReactInVue(BasicReactComponent)
+  },
+  setup() {
+  }
+}
+</script>
