dfaa1add0ce1a582d3c32e30ddbc1444a5965b75
liziwl <leezisy@gmail.com>
2022-09-18 11:02:46 +0800

    rm files

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -2,7 +2,6 @@ import { defineClientConfig } from '@vuepress/client'
 import BusTable from "./components/BusTable.vue";
 import Realtimemap from './components/Realtime-map.vue'
 import WeatherSpan from './components/weather-span.vue'
-import BusChartReact from './components/BusChartReact.vue'
 import BusChartVue from './components/BusChartVue.vue'
 // import AdsenseUnit from './components/adsense-inline-article.vue'
 
@@ -11,7 +10,6 @@ export default defineClientConfig({
     app.component("BusTable", BusTable)
     app.component("Realtimemap", Realtimemap)
     app.component("WeatherSpan", WeatherSpan)
-    app.component("BusChartReact", BusChartReact)
     app.component("BusChartVue", BusChartVue)
     // app.component("AdsenseUnit", AdsenseUnit)
   },
