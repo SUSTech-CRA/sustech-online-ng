8e91d06e066f2b43617e80cfe77c0a4f29812832
liziwl <leezisy@gmail.com>
2022-09-24 17:31:36 +0800

    本地化

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -185,6 +185,23 @@ export default defineUserConfig({
         contributorsText: '贡献者',
         editLink: true,
         docsBranch: 'master',
+        // 本地化
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+        // 404 page
+        notFound: [
+            '这里什么都没有',
+            '我们怎么到这来了？',
+            '这是一个 404 页面',
+            '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleColorMode: '切换夜间模式',
+        toggleSidebar: '切换侧边栏',
         sidebarDepth: 2,
         sidebar: [
             '/',
