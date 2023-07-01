80fca2f7c78f96667c393546d5d0a289e297cf47
liziwl <leezisy@gmail.com>
2023-07-01 10:04:46 +0800

    update ver

--- docs/.vuepress/components/TabView.vue
+++ docs/.vuepress/components/TabView.vue
@@ -1,31 +1,57 @@
 <template>
   <div>
+    <a-config-provider
+    :theme="{
+      token: {
+        colorPrimary: '#00b96b',
+      },
+    }"
+  >
+    <a-button />
     <a-tabs type="card" size="large">
-      <a-tab-pane key="tab1" tab="车辆位置 Bus Realtime Loaction">
+      <a-tab-pane key="tab1">
+        <template #tab>
+          <div class="tab-text">车辆实时位置<br />Bus Realtime Location</div>
+        </template>
         位置每5秒自动刷新。Location refreshes automatically every 5 seconds
         <Realtimemap/>
         <BusChartVue/>
       </a-tab-pane>
-      <a-tab-pane key="tab2" tab="时间表 Timetable">
+      <a-tab-pane key="tab2">
+        <template #tab>
+          <div class="tab-text">时间表<br />Timetable</div>
+        </template>
         <BusTable></BusTable>
       </a-tab-pane>
     </a-tabs>
+  </a-config-provider>
   </div>
 </template>
 
 <script>
 import { Tabs } from 'ant-design-vue';
+import { ConfigProvider } from 'ant-design-vue';
 
 export default {
   name: 'TabView',
   components: {
     ATabs: Tabs,
     ATabPane: Tabs.TabPane,
+    AConfigProvider: ConfigProvider
   }
 };
 </script>
 
 <style>
-@import 'ant-design-vue/es/tabs/style';
+.ant-tabs {
+  color: unset;
+  font-size: unset;
+  font-variant: unset;
+  font-feature-settings: unset;
+}
+
+.tab-text {
+  white-space: pre-line;
+}
 
 </style>
---
+++ docs/transport/bustimer-test.md
@@ -0,0 +1,7 @@
+# 🚌校园巴士时刻表 SUSTech Campus Bus Info
+
+## 路线图 System Map
+
+<a data-fancybox title="" href="https://mirrors.sustech.edu.cn/git/sustech-online/sustech-online-ng/-/raw/master/docs/transport/busline2.png">![](./busline2.png)</a>
+
+<TabView></TabView>
