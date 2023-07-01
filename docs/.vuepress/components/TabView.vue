8ccac6f5a4243573e334631c7631181a02a501ea
liziwl <leezisy@gmail.com>
2023-07-01 10:19:51 +0800

    update format

--- docs/.vuepress/components/TabView.vue
+++ docs/.vuepress/components/TabView.vue
@@ -1,30 +1,27 @@
 <template>
   <div>
-    <a-config-provider
-    :theme="{
+    <a-config-provider :theme="{
       token: {
         colorPrimary: '#00b96b',
       },
-    }"
-  >
-    <a-button />
-    <a-tabs type="card" size="large">
-      <a-tab-pane key="tab1">
-        <template #tab>
-          <div class="tab-text">车辆实时位置<br />Bus Realtime Location</div>
-        </template>
-        位置每5秒自动刷新。Location refreshes automatically every 5 seconds
-        <Realtimemap/>
-        <BusChartVue/>
-      </a-tab-pane>
-      <a-tab-pane key="tab2">
-        <template #tab>
-          <div class="tab-text">时间表<br />Timetable</div>
-        </template>
-        <BusTable></BusTable>
-      </a-tab-pane>
-    </a-tabs>
-  </a-config-provider>
+    }">
+      <a-tabs type="card" size="large">
+        <a-tab-pane key="tab1">
+          <template #tab>
+            <div class="tab-text">车辆实时位置<br />Bus Realtime Location</div>
+          </template>
+          位置每5秒自动刷新。Location refreshes automatically every 5 seconds
+          <Realtimemap />
+          <BusChartVue />
+        </a-tab-pane>
+        <a-tab-pane key="tab2">
+          <template #tab>
+            <div class="tab-text">时间表<br />Timetable</div>
+          </template>
+          <BusTable></BusTable>
+        </a-tab-pane>
+      </a-tabs>
+    </a-config-provider>
   </div>
 </template>
 
@@ -53,5 +50,4 @@ export default {
 .tab-text {
   white-space: pre-line;
 }
-
 </style>
--- docs/.vuepress/components/bus/ObjectSelector.vue
+++ docs/.vuepress/components/bus/ObjectSelector.vue
@@ -1,16 +1,26 @@
 <template>
-  <button
-      v-for="(value, key, index) in objs"
-      :key="index"
-      @click="select(key)"
-      :class="key==keySelected? 'selected' : 'unselected'"
-  >
-    {{ key }}
-  </button>
+  <a-config-provider :theme="{
+    token: {
+      colorPrimary: '#00b96b',
+    },
+  }">
+    <a-button v-for="(value, key, index) in objs" :key="index" @click="select(key)"
+      :class="key == keySelected ? 'selected' : 'unselected'" :type="key == keySelected ? 'primary' : 'default'" size="large">
+      {{ key }}
+    </a-button>
+  </a-config-provider>
+
   <slot v-bind:selected="objs[keySelected]"></slot>
 </template>
 <script>
+import { ConfigProvider } from 'ant-design-vue';
+import { Button as AButton } from 'ant-design-vue';
+
 export default {
+  components: {
+    AButton,
+    AConfigProvider: ConfigProvider
+  },
   data() {
     return {
       keySelected: null,
@@ -47,22 +57,22 @@ export default {
 button {
   padding: 10px 15px;
   margin: 3px 3px 3px 3px;
-  background-color: white;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 14px;
-  border-radius: 2px;
-  border: 1px solid rgb(194, 194, 194);
+  // background-color: white;
+  // text-align: center;
+  // text-decoration: none;
+  // display: inline-block;
+  // font-size: 14px;
+  // border-radius: 2px;
+  // border: 1px solid rgb(194, 194, 194);
 }
 
 .selected {
-  border-color: rgb(64, 169, 255);
-  background-color: rgb(64, 169, 255);
-  color: white;
+  // border-color: rgb(64, 169, 255);
+  // background-color: rgb(64, 169, 255);
+  // color: white;
 }
 
 .unselected {
-  color: #000;
+  // color: #000;
 }
 </style>
